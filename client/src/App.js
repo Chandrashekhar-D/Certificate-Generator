0daad538a3c28aa75fd90a439daae489d318d47d
chandra-d1 <umailtochr@gmail.com>
2021-07-23 12:08:23 +0530

    Added nodemailer

---
+++ client/src/App.js
@@ -0,0 +1,255 @@
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { ChakraProvider, Stack, Box, Button, Center, Input } from "@chakra-ui/react";
+import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+import domtoimage from 'dom-to-image';
+import destr from 'destr';
+
+function App() {
+  function uploaded() {
+    document.getElementById('name').style.position = 'absolute'
+    document.querySelector("#name").style.top = "100px";
+    document.querySelector("#name").style.left = "300px";
+    document.getElementById("name").style.fontSize = "2rem";
+    document.getElementById("name").style.fontweight = "bold";
+    document.getElementById("name").style.color = "black";
+  }
+
+
+  const move_down = ()=> {
+    document.getElementById("name").style.top = parseInt(document.getElementById("name").style.top) + 10 +"px";
+  }
+  const move_up =()=> {
+    document.getElementById("name").style.top = parseInt(document.getElementById("name").style.top) - 10 +"px";
+    }
+  
+  const move_left =()=> {
+    document.getElementById("name").style.left = parseInt(document.getElementById("name").style.left) - 10 +"px";
+  }
+  const move_right =()=> {
+    document.getElementById("name").style.left = parseInt(document.getElementById("name").style.left) + 10 +"px";
+  }
+  const fontplus =()=> {
+    document.getElementById('name').style.fontSize=parseFloat(document.getElementById('name').style.fontSize)+0.1+"rem";
+  }
+  const fontmin =() =>{
+    document.getElementById('name').style.fontSize=parseFloat(document.getElementById('name').style.fontSize)-0.1+"rem";
+
+  }
+  const changeColor = ()=>{
+    
+    document.getElementById("name").style.color = "white";
+  }
+
+  const convert = ()=>{
+    html2canvas(document.querySelector("#capture")).then(canvas => {
+      var image = canvas.toDataURL("image/png").replace("image/png","image/octet-stream");
+      window.location.href = image;
+  });
+  }
+
+  const download =()=>{
+    var node = document.getElementById("capture");
+    domtoimage.toPng(node).then(function(dataUrl){
+      // var img = new Image();
+      // img.src = dataUrl;
+      // downloadURI(dataUrl, "image.png")
+  // console.log("This is dataurl",dataUrl)
+
+      setSendData(dataUrl)
+      // console.log("This is",typeof(sendData))
+    }).catch((err)=>{
+      console.log(err);
+    })
+  }
+
+
+  function downloadURI(uri,name){
+    var link = document.createElement("a");
+    link.download = name;
+    link.href = uri;
+    document.body.appendChild(link)
+    link.click();
+    document.body.removeChild(link);
+  }
+
+  const [image, setImage] = useState();
+  const [name, setName] = useState();
+  const [preview, setPreview] = useState();
+
+  useEffect(() => {
+    if (image) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setPreview(reader.result);
+      };
+      reader.readAsDataURL(image);
+    } else {
+      setPreview(null);
+    }
+  }, [image]);
+
+  const [csvData,setCsvData] = useState("");
+  const [sendData,setSendData] = useState("");
+
+const  post = async (e)=>{
+  e.preventDefault();
+  download()
+  // console.log(sendData + "Lol")
+  // console.log(typeof sendData)
+  //  console.log("image",sendData)
+  // console.log("csv",csvData)
+  // download();
+  // console.log(typeof(destr(sendData)))
+  const res = await fetch("/api",{
+    method:"POST",
+    headers:{
+      'Content-Type':"application/json"
+    },
+    body: JSON.stringify({"csvData":csvData,"img":"https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fb%2Fb6%2FImage_created_with_a_mobile_phone.png%2F1200px-Image_created_with_a_mobile_phone.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FImage&tbnid=gxFxsvFBmxeZ9M&vet=12ahUKEwix7PHvsPfxAhUTFSsKHa2eCMwQMygAegUIARDJAQ..i&docid=0JWe7yDOKrVFAM&w=1200&h=900&q=image&ved=2ahUKEwix7PHvsPfxAhUTFSsKHa2eCMwQMygAegUIARDJAQ"})
+  });
+  console.log(res);
+}
+
+
+// var node = document.getElementById("capture");
+//   domtoimage.toPng(node).then(function(dataUrl){
+//     // var img = new Image();
+//     // img.src = dataUrl;
+//     // downloadURI(dataUrl, "image.png")
+//     setSendData(dataUrl)
+//   }).catch((err)=>{
+//          console.log(err);
+//    })
+
+
+// let data = sendData;
+//   console.warn(data)
+//   fetch("http://localhost:5000/",{
+//     method:'POST',
+//     headers:{
+//       'Accept':'application/json',
+//       'Content-Type': 'application/json'
+//     },
+//     body:JSON.stringify(data)
+//   }).then((result)=>{
+//     console.warn(result)
+//   })
+
+
+
+var datatosend=[];
+var retrieveddata=[];
+var uploadedtheCSV=false;
+
+const readCSV =(event)=>{
+    
+  console.log(event.target.files[0]);
+  uploadedtheCSV=true;
+  const reader =new FileReader()
+  reader.onload=function (e){
+    const text1=e.target.result;
+    retrieveddata= text1.split('\r\n')
+    datatosend.push(text1);
+    setCsvData(retrieveddata)
+  }
+  reader.readAsText(event.target.files[0])
+}
+
+
+  return (
+    <ChakraProvider>
+      <Stack direction={["column", "row"]} spacing="24px">
+        <Center>
+          <Box w="220px" h="600px" >
+            <form method="POST">
+          <Input
+          mt={10} mx={4}
+                type="text"
+                name=""
+                placeholder="Enter your name"
+                
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            <Button mx={4} my={2} onClick={changeColor}> Change Color</Button>
+            <Button mx={4} my={2} mt={2} onClick={move_up} colorScheme="blue">
+              Move Up
+            </Button>{" "}
+            <br />
+            <Button mx={4}  my={2} onClick={move_down} colorScheme="blue">
+              Move Down
+            </Button>{" "}
+            <br />
+            <Button mx={4} my={2}  onClick={move_left} colorScheme="blue">
+              Move Left
+            </Button>{" "}
+            <br />
+            <Button mx={4} my={2}  onClick={move_right} colorScheme="blue">
+              Move Right
+            </Button>{" "}
+            <br />
+            <Button mx={4} my={2}  onClick={fontplus} colorScheme="blue">
+              Increase Font
+            </Button>{" "}
+            <br />
+            <Button mx={4} my={2}  onClick={fontmin} colorScheme="blue">
+              Decrease Font
+            </Button>{" "}
+            <br />
+            <Input
+            mx={4} my={2}
+                type="file"
+                name="csv"
+                onChange={readCSV} />
+            {/* <Button colorScheme="red" mx={2} onClick={download}> Generate Certificates</Button> */}
+            <Button type="button" onClick={post}> Send Data </Button>
+
+            </form>
+          </Box>
+        </Center>
+        <Center>
+          <Box w="980px" h="">
+            <div className="App">
+              <input
+                type="file"
+                name="template"
+                id=""
+                accept="image/*"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+                  if (file) {
+                    setImage(file);
+                    uploaded();
+                  } else {
+                    setImage(null);
+                  }
+                }}
+              />
+            </div>
+            <br />
+            <div id="capture">
+                {name ? <p id="name">{name}</p> : <p id="name"></p>}
+                {preview ? <img src={preview} alt="" id="template" /> : null}
+              </div>
+          </Box>
+          
+        </Center>
+      </Stack>
+
+              {/* <form id="form" method="POST" >
+                <input type="text" value={sendData} name="name" id="data" onChange={(e)=>{
+                  document.getElementById("data").style.color="black"
+                  setSendData(e.target.value)
+                }} />
+                <button type="button" onClick={post}> Send Data </button>
+              </form> */}
+
+    </ChakraProvider>
+
+  );
+}
+
+export default App;
---
+++ server/router/auth.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const upload = require('express-fileupload')
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const csv = require('fast-csv');
+const nodemailer = require('nodemailer');
+
+
+router.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+// router.post('/',(req,res)=>{
+//     var file = req.files.file
+//     var csv= req.files.csv
+//     console.log(file.name)
+//     console.log(csv.name);
+//     res.send({message:"File Uploaded"})
+//     file.mv("./uploads/"+file.name);
+//     file.mv("./uploads/"+csv.name);
+// })
+
+router.post('/api',async(req,res)=>{
+
+    const data = req.body;
+    // console.log(req.body.csvData)
+    // console.log(req.body.key)
+    console.log('string',data);
+
+    // console.log(typeof data)
+    console.log(typeof data.csvData)
+    console.log(data.csvData)
+    var csvobj=data.csvData
+    // data.splice(-1)
+
+    var arrayLength = csvobj.length;
+    var address=""
+for (var i = 0; i < arrayLength; i=i+1) {
+    console.log((csvobj[i]).split(",")[2])
+    address=(csvobj[i]).split(",")[2]+","+address
+    str = address.replace(/,\s*$/, "");
+    //Do something
+}
+
+console.log(address)
+console.log(str)
+
+
+ 
+ 
+
+    // console.log(data.key);
+    // console.log(typeof(data))
+    // res.json({message:req})
+
+    
+    // req.files.file.mv("./uploads"+filen2,function(err){
+    //     if(err){
+    //         console.log("error")
+    //     } else{
+    //         console.log("file uploaded")
+    //     }
+    // })
+    // const email = req.body;
+    //  console.log(email.email)
+        // create reusable transporter object using the default SMTP transport
+    
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+        user: 'dummyacc012345689@gmail.com', // generated ethereal user
+        pass: 'dummy@321', // generated ethereal password
+        },
+    });
+    const msg = {
+        from: '" from dsc 👻" <dummy0123456789@gmail.com>',
+        to: address, 
+        subject: "Hello ✔", // Subject line
+        text: "Hello world?", // plain text body
+        html: "<b>Hello world?</b>, ", // html body
+        html:"Embedded image: <img src='cid:unique@nodemailer.com'/>",
+        attachments: [{
+            filename: 'image.png',
+            path: data.img
+        }]
+    }
+
+    // send mail with defined transport object
+    let info = await transporter.sendMail(msg);
+
+    console.log("Message sent: %s", info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+        console.log("email sent")
+})
+
+
+module.exports = router;
