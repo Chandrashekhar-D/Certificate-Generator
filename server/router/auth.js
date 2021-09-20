5593f9b62800b8c3fb707fd6a5baaeb4dfe8c348
Vedant Gokhale <gokhalevedant06@gmail.com>
2021-09-20 16:34:18 +0530

    backend image render, move template in uploads folder,fetch image for certificate generation done

---
+++ server/router/auth.js
@@ -0,0 +1,107 @@
+const express = require("express");
+const router = express.Router();
+const fs = require("fs");
+const path = require("path");
+const csv = require("fast-csv");
+const nodemailer = require("nodemailer");
+const Image = require("purified-image");
+const { dirname } = require("path");
+
+
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+// To move file into server folder
+router.post('/upload',async (req,res)=>{
+    try {
+        const file = req.files.template;
+        const savePath = path.join(__dirname,'../','uploads',file.name)
+        await file.mv(savePath)
+        console.log("Image Uploaded")
+        res.status(200).json({
+          success: "success",
+        });
+    } catch (error) {
+        console.log(error);
+        res.send("Error uploading file")
+    }
+})
+
+
+router.post("/api", async (req, res) => {
+  const data = req.body;
+  let senderEmail = data.Email;
+  let senderPassword = data.Password;
+  let csvobj = await data.csvData;
+  let csvNames = [];
+  let csvEmails = [];
+  let name = [];
+  var arrayLength = csvobj.length;
+  var address = "";
+  for (var i = 0; i < arrayLength - 1; i = i + 1) {
+    console.log(csvobj[i].split(",")[2]);
+    address = csvobj[i].split(",")[2] + "," + address;
+    str = address.replace(/,\s*$/, "");
+  }
+  console.log("Sender Email : ",senderEmail);
+  console.log()
+  console.log("Sender Password : ",senderPassword);
+  console.log()
+  console.log("CSV Data : ",csvobj);
+  console.log()
+
+
+
+  //To render image at backend
+let image = new Image(`uploads/temp.jpg`);
+image
+.loadFont('fonts/OpenSans.ttf')
+.draw(ctx => {
+    ctx.fillStyle = '#000000';
+    ctx.font = '50 Open Sans';
+    ctx.fillText('example', 100, 500);
+})
+.save(`out/certificate.jpg`)
+.then(() => console.log('Image Saved'))
+.catch((err)=>console.log(err));
+
+  const transporter = await nodemailer.createTransport({
+      service: "gmail",
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+      user: 'dummyacc012345689@gmail.com', // generated ethereal user
+      pass: 'dummy@321', // generated ethereal password
+      // user: `${senderEmail}`, // generated ethereal user
+      // pass: `${senderPassword}`, // generated ethereal password
+      },
+  });
+  const msg = {
+      from: '" from dsc 👻" <dummy012345689@gmail.com>',
+      to: address,
+      subject: "Hello ✔", // Subject line
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent turpis neque, placerat sed vestibulum sit amet, lacinia eget ante. Vivamus pulvinar eu eros quis rhoncus. Pellentesque interdum risus quis urna posuere auctor et sit amet eros. Suspendisse facilisis lacinia eros sed accumsan. Ut rhoncus orci sit amet tempus pulvinar. ", // plain text body
+      html: "<h1>Congratulations you have earned Certificate</h3>  ", // html body
+      attachments: [{
+          filename: 'certificate.png',
+          path: `out/certificate.jpg`
+      }]
+  }
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail(msg);
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+      console.log("email sent")
+});
+
+
+
+module.exports = router;
